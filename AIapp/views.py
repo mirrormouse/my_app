54e183601436c0e760a1ac58a8b92c6f38ad4ce5
mirrormouse <kagamidokei@gmail.com>
2022-04-04 19:02:41 +0900

    AIinitial

--- AIapp/views.py
+++ AIapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tensorflow.keras.models import load_model
 # Create your views here.
 def index(request):
+    model = load_model("model/my_model.h5")
     return HttpResponse('OK!')
